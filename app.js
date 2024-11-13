6e7bea910a6ddb9f51ea0f57d4f1f245d77584c6
Raj Lucky <raj376457@gmail.com>
2024-11-13 22:09:11 +0530

    commit!

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const MongoStore = require('connect-mongo'); // Import MongoStore
 
 dotenv.config();
 
@@ -12,12 +13,23 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true }));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('MongoDB connection error:', error));
+
+// Configure session to use MongoDB for storing sessions
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI, // MongoDB URI for session storage
+        ttl: 14 * 24 * 60 * 60 // Session TTL (14 days)
+    }),
+    cookie: { secure: false } // Set to true in production if using HTTPS
+}));
 
 // Register routes
 app.use('/spotify', spotifyRoutes);
