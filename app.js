36e389125068d258b5d3fd395044fab2bf1298c5
Raj Lucky <raj376457@gmail.com>
2024-11-14 13:15:58 +0530

    commit!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
-const MongoStore = require('connect-mongo'); // Import MongoStore
+const MongoStore = require('connect-mongo'); 
 
 dotenv.config();
 
@@ -14,30 +14,29 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-// MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('MongoDB connection error:', error));
 
-// Configure session to use MongoDB for storing sessions
+
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI, // MongoDB URI for session storage
-        ttl: 14 * 24 * 60 * 60 // Session TTL (14 days)
+        mongoUrl: process.env.MONGO_URI, 
+        ttl: 14 * 24 * 60 * 60 
     }),
-    cookie: { secure: false } // Set to true in production if using HTTPS
+    cookie: { secure: true } 
 }));
 
-// Register routes
+
 app.use('/spotify', spotifyRoutes);
 app.use('/api', userRoutes);
 
-// Home route
+
 app.get('/', (req, res) => res.render('index'));
 
-// Start server
+
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
